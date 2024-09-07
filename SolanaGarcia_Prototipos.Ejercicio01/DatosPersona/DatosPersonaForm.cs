b9125e304116b583d1c2ba9740c0fdd882afd514
solana garcia <solanagarcia01@gmail.com>
2024-09-07 17:19:10 -0300

    DatosPersona - Esqueleto

---
+++ SolanaGarcia_Prototipos.Ejercicio01/DatosPersona/DatosPersonaForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolanaGarcia_Prototipos.Ejercicio01.DatosPersona
+{
+    public partial class DatosPersonaForm : Form
+    {
+        private DatosPersonaModel modelo = new();
+        public DatosPersonaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
