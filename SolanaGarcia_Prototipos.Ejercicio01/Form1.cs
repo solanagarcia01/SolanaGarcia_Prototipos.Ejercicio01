dd86b0b166a2b3976b32585e8170a0aaffe30631
solana garcia <solanagarcia01@gmail.com>
2024-09-07 16:43:41 -0300

    Agregar archivos de proyecto.

---
+++ SolanaGarcia_Prototipos.Ejercicio01/Form1.cs
@@ -0,0 +1,10 @@
+namespace SolanaGarcia_Prototipos.Ejercicio01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
